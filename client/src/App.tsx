f60b10bf76be00321b5998614e19b1eb43c69695
Ajmal Ansari <ansari_ajmal@hotmail.com>
2026-08-25 16:57:59 +0300

    Fixed scroll to top issue

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,10 +12,12 @@ import Contact from "@/pages/Contact";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import Chatbot from "@/components/Chatbot";
+import ScrollToTop from "@/components/ScrollToTop";
 
 function Router() {
   return (
     <div className="min-h-screen flex flex-col">
+      <ScrollToTop />
       <Navbar />
       <main className="flex-1">
         <Switch>
